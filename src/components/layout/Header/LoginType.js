eda6951358891f8c1f935cf1161b5a7442d5da78
Dominic Tremblay <dominic@nutribe.ca>
2016-06-21 17:52:12 -0400

    added the logout

--- src/components/layout/Header/LoginType.js
+++ src/components/layout/Header/LoginType.js
@@ -3,6 +3,19 @@ import {Link} from 'react-router';
 
 class LoginType extends Component {
 
+  logout() {
+    let currentUser = this.props.currentUser;
+    if (Object.keys(currentUser).length === 0 && currentUser.constructor === Object)
+      return;
+    else
+      return(<a href='#' onClick={this.clearLogout}>Log out</a>);
+  }
+
+  clearLogout() {
+    console.log("logging out")
+    localStorage.clear();
+  }
+
   getLink() {
     let currentUser = this.props.currentUser;
     if (Object.keys(currentUser).length === 0 && currentUser.constructor === Object)
@@ -14,7 +27,7 @@ class LoginType extends Component {
   render() {
 
     return (
-      <div>{this.getLink()}</div>
+      <div>{this.getLink()} {this.logout()}</div>
        
     )
   }
